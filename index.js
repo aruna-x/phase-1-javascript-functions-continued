32cca0832559c281c3f2d2ca9e280ac35f8ab470
Aruna <aruna.evan15@gmail.com>
2021-09-21 10:27:01 -0700

    commit tree 4081f47da48bc347596b27642c972ef6bd03de4d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol="*") {
+    return ( (param="special") => {return `You are ${symbol}${param}${symbol}!`} )
+}
